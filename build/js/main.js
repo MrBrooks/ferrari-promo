cd186298b3fee19b8c8d944b88cb84f1ab949149
Denis Kulikov <altingfest@gmail.com>
2016-09-20 14:39:00 +0300

    add some anims

--- build/js/main.js
+++ build/js/main.js
@@ -14681,6 +14681,9 @@ $(document).ready(function() {
       mores.on('click',function(){
         $(popups.get(parseInt($(this).attr('data-index')) - 1)).addClass('active');
       });
+      close_btns.on('click',function(){
+        popups.removeClass('active');
+      });
 
     });
 
@@ -14695,9 +14698,155 @@ $(document).ready(function() {
  
   DynamicGrid();
   ProductView();
+  var scroll_anim = new AnimOnScroll({
+    selector: '.slide-from-down',
+    visible: 'active'
+  });
 
   var sliders = $('.slider').owlCarousel({
     dotsContainer: ".slider-preview",
     items: 1
   });
+
+
+  var window_updater = new WindowUpdater([
+    {
+      event: "scroll",
+      actions: [
+        scroll_anim.updateView,
+        // video_play.scrollControl,
+        // econtenta_pixel.checkScrollConditions
+      ]
+    },
+    {
+      event: "resize",
+      actions: [
+        // anim_on_scroll.updateItems,
+        // full_height.update
+      ]
+    }
+  ]);
+
+  $(window).trigger('scroll');
 });
+
+
+
+//JS CLASSES
+
+function WindowUpdater(opts){ 
+  var self = this, timer;
+  // var opt = [
+  //   {
+  //     event: 'scroll',
+  //     actions: [] 
+  //   },
+  //   {
+  //     event: 'resize',
+  //     actions: []
+  //   }s
+  // ];
+  self.add = function(event, func){
+    for(var i = 0; i < opts.length; i++){
+      if(opts[i].event === event){
+        opts[i].actions.push(func);
+        break;
+      }
+    }
+  };
+
+  self.update = function(event){
+    clearTimeout(timer);
+    timer = setTimeout(function(){
+      // console.log(event);
+      if(event.data !== null){
+        for(var i = 0; i < opts[event.data].actions.length; i++){
+          opts[event.data].actions[i]();
+        }
+      }
+      //do smthng
+    },10);
+  };
+
+  self.onEvents = function(){
+    for(var i = 0; i < opts.length; i++){
+      $(window).on(opts[i].event, i, self.update);
+    }
+  };
+
+  // self.initScroll = function(){
+  // };
+
+  // self.initResize = function(){
+  // };
+
+  // self.initAll = function(){
+  // };
+  self.onEvents();
+}
+
+function AnimOnScroll(options){
+  var def = {
+    selector: ".scroll-anim",
+    visible: "visible"
+  };
+
+  var self = this;
+  var opt = $.extend(def, options);
+
+  var select = $(opt.selector);
+  var items = [];
+  var H = $(window).height(), if_mobile = $(window).width() < 768? true : false;
+
+  
+  function isHidden(elem){
+    var ws = $(window).scrollTop();
+    if( (elem.top >= ws + H) || (elem.top + elem.height <= ws)){
+      return true;
+    }
+    return false;
+  }
+
+  function isVisible(elem){
+    var ws = $(window).scrollTop();
+    if( ((elem.top > ws) && (elem.top < ws+H)) || (elem.top + elem.height > ws && elem.top < ws)){
+      return true;
+    }
+    return false;
+  }
+
+  function isFullyVisible(elem){
+    var ws = $(window).scrollTop();
+    if(elem.top > ws && elem.top + elem.height < ws + H){
+      return true;
+    }
+    return false;
+  }
+
+  self.updateItems = function (){
+    items = [];
+    H = $(window).height();
+    select.each(function(){
+      items.push({
+        item: $(this),
+        top: $(this).offset().top,
+        height: $(this).height()
+      });
+    });
+  };
+
+  self.updateView = function(){
+    for(var i = 0; i < items.length; i++){
+      if(!items[i].item.hasClass(opt.visible)){
+        if(isVisible(items[i])){
+          items[i].item.addClass(opt.visible);
+        }
+      }
+    }
+  };
+  self.updateItems();
+
+  if(if_mobile){
+    select.addClass(opt.visible);
+  }
+}
