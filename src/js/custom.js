b5a346fc1c800d26613fb40f7c85983f9d0ec99b
Denis Kulikov <altingfest@gmail.com>
2016-09-21 17:03:29 +0300

    update animations

--- src/js/custom.js
+++ src/js/custom.js
@@ -74,10 +74,10 @@ $(document).ready(function() {
     items: 1
   });
 
-  var vid = document.getElementById("ferrari-video");
-  vid.onended = function(){
-    $(this).css('opacity', 0);
-  };
+  // var vid = document.getElementById("ferrari-video");
+  // vid.onended = function(){
+  //   $(this).css('opacity', 0);
+  // };
 
 
   var window_updater = new WindowUpdater([
@@ -226,6 +226,7 @@ function AutoPlay(options){
   var def = {
     selector: ".auto-play",
     class: 'active',
+    prev: 'prev',
     pause: 7000
   };
   var self = this;
@@ -238,14 +239,17 @@ function AutoPlay(options){
     count = items.length;
     iter = 0;
     if(count > 0){
-      $(items.get(iter++)).addClass(opt.class);
+      $(items.get(iter)).addClass(opt.class);
     }
   }
 
   self.start = function (){
     if (count > 0){
       timer = setInterval(function(){
-        $(items.get(iter++ % count)).addClass(opt.class).siblings().removeClass(opt.class);
+        $(items).removeClass(opt.prev);
+        $(items.get(iter % count)).addClass(opt.prev);
+        $(items.get(++iter % count)).addClass(opt.class).siblings().removeClass(opt.class);
+
       },opt.pause);
     }
   };
